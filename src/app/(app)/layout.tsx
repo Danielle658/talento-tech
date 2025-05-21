d0cf0acd75c61743542ff91dd68c8e8bb5f50c03
MoneyWise <moneywise057@gmail.com>
2025-05-21 22:33:16 +0000

    corrigir para que ele pegue a tela toda

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -186,7 +186,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
               </DropdownMenu>
             </div>
           </header>
-          <main className="flex-1 p-4 md:p-6 lg:p-8">
+          <main className="flex flex-1 flex-col p-4 md:p-6 lg:p-8">
             {children}
           </main>
         </SidebarInset>
