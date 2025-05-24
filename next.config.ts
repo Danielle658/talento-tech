33e0573357a8cd8f864678556f9c39e69b7942a7
MoneyWise <moneywise057@gmail.com>
2025-05-24 14:30:22 +0000

    corrigir erro de abrir o site do aplicativo

--- next.config.ts
+++ next.config.ts
@@ -19,13 +19,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    allowedDevOrigins: [
-      "http://localhost:9006", 
-      "https://9006-firebase-studio-1747837393667.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev", 
-      "http://9006-firebase-studio-1747837393667.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev"
-    ],
-  },
+  // A seção experimental foi removida.
   // A seção rewrites foi removida pois a email-api não está mais em uso.
 };
 
